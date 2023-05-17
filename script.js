3adc0ac8dd1cb1fedd8102a3be820ae4f40b50f3
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-05-17 15:20:46 +0200

    setInterval if inactive boxes

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const slide = document.querySelector(".classes-box");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
 
-nextButton.addEventListener("click", () => {
-    let slideWidth = slide.clientWidth + 3;
-    slidesContainer.scrollLeft += slideWidth;
-});
+let slideWidth = slide.clientWidth + 3;
+let intervalId;
 
-prevButton.addEventListener("click", () => {
-    let slideWidth = slide.clientWidth + 3;
-    slidesContainer.scrollLeft -= slideWidth;
+// Move to the next slide
+function moveToNextSlide() {
+    slidesContainer.scrollLeft += slideWidth;
+}
+// Reset the interval timer
+function resetInterval() {
+    clearInterval(intervalId);
+    intervalId = setInterval(moveToNextSlide, 5000);
+}
+// Reset interval on click on one of the arrows
+nextButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft += slideWidth;
+    resetInterval();
 });
+prevButton.addEventListener("click", () => {
+    slidesContainer.scrollLeft -= slideWidth;
+    resetInterval();
+});
+// Reset interval if scroll on boxes
+slidesContainer.addEventListener("scroll", resetInterval);
+// Start the interval interval
+intervalId = setInterval(moveToNextSlide, 2000);
