6339e0de6e75dfa20d780badf09ddc486ed94cd0
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-06-23 16:44:39 +0200

    reorganized folders

--- includes/header.php
+++ includes/header.php
@@ -1,7 +1,6 @@
 <header class="header-nav">
     <div class="header-nav-ttl">
-
-        <a href="index.php"><img href="index.php" class="header-logo" src="img/livre-logo.png" alt="logo" /></a>
+        <a href="index.php"><img href="../index.php" class="header-logo" src="img/livre-logo.png" alt="logo" /></a>
         <h1 class="header-ttl">Polyglossia</h1>
     </div>
     <input id="header-menu-toggle" type="checkbox" />
@@ -9,12 +8,12 @@
         <div class="header-menu-button"></div>
     </label>
     <ul class="header-menu">
-        <li><a class="header-lnk" href="index.php">Accueil</a></li>
+        <li><a class="header-lnk" href="PHP/index.php">Accueil</a></li>
         <li><a class="header-lnk" href="#">Formations</a></li>
         <li><a class="header-lnk" href="#">Qui sommes-nous</a></li>
         <li><a class="header-lnk" href="#">Contacts</a></li>
         <li class="header-cta-desktop cta-desktop">
-            <a class="header-cta cta" href="connexion.php">Se connecter</a>
+            <a class="header-cta cta" href="login.php">Se connecter</a>
         </li>
     </ul>
 </header>
