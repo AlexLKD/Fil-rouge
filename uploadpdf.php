91f55f6258d8f1d42d2e6b154651a18fcb0604a2
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-07-17 12:54:43 +0200

    resolve csrf error on dashboard page

--- uploadpdf.php
+++ uploadpdf.php
@@ -3,11 +3,11 @@ require 'includes/_database.php';
 session_start();
 
 if (!(array_key_exists('HTTP_REFERER', $_SERVER)) && str_contains($_SERVER['HTTP_REFERER'], $_ENV["URL"])) {
-    header('Location: index.php?msg=error_referer');
+    header('Location: dashboard.php?msg=error_referer');
     exit;
 } else if (!array_key_exists('token', $_SESSION) || !array_key_exists('token', $_REQUEST) || $_SESSION['token'] !== $_REQUEST["token"]) {
     //...
-    header('Location: index.php?msg=error_csrf');
+    header('Location: dashboard.php?msg=error_csrf');
     exit;
 }
 
