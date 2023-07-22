479483486fe4afc5213b2c3400dc550d63ffb706
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-07-22 22:12:06 +0200

    flags & img in gitignore + login_style.css added to affected pages

--- index.php
+++ index.php
@@ -7,17 +7,6 @@ $_SESSION['token'] = md5(uniqid(mt_rand(), true));
 
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-
-<head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Document</title>
-    <link rel="stylesheet" href="CSS/style.css" />
-</head>
-
 <body>
     <?php
     require 'includes/header.php';
---
+++ language.php
@@ -0,0 +1,10 @@
+<?php
+require 'includes/_database.php';
+session_start();
+?>
+
+<body>
+    <?php
+    include 'includes/header.php';
+    ?>
+</body>
