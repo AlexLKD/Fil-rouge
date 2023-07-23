7f371bab4f96ae62ed261545537481598829d89b
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-07-23 17:40:26 +0200

    upload pdf also send the course informations in the database

--- uploadpdf.php
+++ uploadpdf.php
@@ -21,6 +21,16 @@ if ($fileType != "pdf") {
     exit;
 }
 if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+    $title_course = $_POST['title_course'];
+    $id_difficulty = $_POST['id_difficulty'];
+    $id_languages = $_POST['id_languages'];
+    $query = $dbCo->prepare("INSERT INTO course (id_course, date_course, title_course, id_difficulty, id_languages) 
+    VALUES (NULL, NOW(), :title_course, :id_difficulty, :id_languages)");
+    $isOk = $query->execute([
+        ':title_course' => $title_course,
+        ':id_difficulty' => $id_difficulty,
+        ':id_languages' => $id_languages
+    ]);
     header('Location: dashboard.php?msg=' . urlencode('The file "' . htmlspecialchars(basename($_FILES["fileToUpload"]["name"])) . '" has been uploaded.'));
     exit;
 } else {
