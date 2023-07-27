79b255898e963de437a6b8b109a633463a0baf3f
Alexandre DUBREZ <adubrez1409@gmail.com>
2023-07-27 13:45:00 +0200

    connection form working

--- login.php
+++ login.php
@@ -25,41 +25,36 @@ session_start();
     <!-- Pills content -->
     <div class="tab-content">
         <div class="tab-pane fade show active" id="pills-login" role="tabpanel" aria-labelledby="tab-login">
-            <form>
+            <form action="actions.php" method="POST">
                 <input type="hidden" name="token" value="<?= $_SESSION['token'] ?? '' ?>">
                 <!-- Email input -->
                 <div class="form-outline mb-4">
-                    <input type="email" id="loginName" class="form-control" />
-                    <label class="form-label" for="loginName">Email or username</label>
+                    <input type="email" id="loginName" class="form-control" name="loginName" required />
+                    <label class="form-label" for="loginName">Email</label>
                 </div>
 
                 <!-- Password input -->
                 <div class="form-outline mb-4">
-                    <input type="password" id="loginPassword" class="form-control" />
-                    <label class="form-label" for="loginPassword">Password</label>
+                    <input type="password" id="loginPassword" class="form-control" name="loginPassword" required />
+                    <label class="form-label" for="loginPassword">Mot de passe</label>
                 </div>
 
                 <!-- 2 column grid layout -->
                 <div class="row mb-4">
                     <div class="col-md-6 d-flex justify-content-center">
-                        <!-- Checkbox -->
-                        <div class="form-check mb-3 mb-md-0">
-                            <input class="form-check-input" type="checkbox" value="" id="loginCheck" checked />
-                            <label class="form-check-label" for="loginCheck">
-                                Remember me
-                            </label>
-                        </div>
-                    </div>
-
-                    <div class="col-md-6 d-flex justify-content-center">
                         <!-- Simple link -->
-                        <a href="change_password.php">Forgot password?</a>
+                        <a href="change_password.php">Mot de passe oublié ?</a>
                     </div>
+                    <?php
+                    if (array_key_exists('msg', $_GET)) {
+                        echo '<p class="connection-info">' . $_GET['msg'] . '</p>';
+                    }
+                    ?>
                 </div>
 
                 <!-- Submit button -->
-                <button type="submit" class="btn btn-primary btn-block mb-4">
-                    Sign in
+                <button type="submit" class="btn btn-primary btn-block mb-4" name="login">
+                    Login
                 </button>
             </form>
         </div>
